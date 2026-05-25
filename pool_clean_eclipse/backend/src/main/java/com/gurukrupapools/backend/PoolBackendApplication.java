b88c8d15e3d1ed9b68e4f498d2b720445e30c563
ALOK ABHISHEK <alokabhi9294@gmail.com>
2026-05-25 14:09:09 +0530

    Update PoolBackendApplication.java

--- pool_clean_eclipse/backend/src/main/java/com/gurukrupapools/backend/PoolBackendApplication.java
+++ pool_clean_eclipse/backend/src/main/java/com/gurukrupapools/backend/PoolBackendApplication.java
@@ -2,8 +2,10 @@ package com.gurukrupapools.backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication
+@EnableAsync
 public class PoolBackendApplication {
 
     public static void main(String[] args) {
